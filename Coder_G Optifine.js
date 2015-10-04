416cd548e772d9249ec27dce2ef2fa9d10ec158c
XxCoderGamerxX <dreamerfead739@gmail.com>
2015-10-04 14:41:49 +0800

    Update Coder_G Optifine.js

--- Coder_G Optifine.js
+++ Coder_G Optifine.js
@@ -3,6 +3,148 @@
 //I can teach you so Don't steal someone's code//
 //twitter: @Coder_G//
 
+//UPDATE CODE//
+//Update Code 
+ 
+var version="1.2";
+var checkForUpdate=false;
+var updateWindow=false;
+var newUpdate;
+var updateMod; 
+var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get(); 
+ 
+ function checkVersion() {
+    var r  = new java.lang.Runnable() {
+        run: function() {
+            try {
+                var urls= new java.net.URL(" https://raw.githubusercontent.com/XxCoderGamerxX/Advanced-Mods/master/Coder_G%20Optifine.txt ");
+                var check = urls.openConnection();
+                check.setRequestMethod("GET");
+                check.setDoOutput(true);
+                check.connect();
+                check.getContentLength();
+                var script = check.getInputStream();
+                var typeb = java.lang.reflect.Array.newInstance(java.lang.Byte.TYPE, 1024);
+                var byteCount = 0; 
+                var checkedVersion;
+                while((byteCount = script.read(typeb)) != -1) { 
+                    checkedVersion = new java.lang.String(typeb, 0, byteCount);               
+                }
+                newUpdate = checkedVersion;
+                if(version+"\n" != checkedVersion) {
+                    clientMessage("§6Advanced Coder_G OPTIFINE has an update! " + newUpdate);
+                    updateWindow=true;
+                }
+                else if(version+"\n"==checkedVersion){
+                clientMessage("There aren't any updates right now");
+                }
+            }
+            catch(err) {
+                clientMessage("Update check failed ");
+                if(err=="JavaException: java.net.UnknownHostException: raw.githubusercontent.com") {
+                                clientMessage("[ACOM] Cannot connect to the internet.");
+                            }
+                            else {
+                                clientMessage("[ACOM]   Error: \n" + err);
+                            } 
+            }
+        }
+    }
+    var threadt = new java.lang.Thread(r);
+    threadt.start();
+}
+function updateVersion() {
+    try {
+        var upd = new android.app.AlertDialog.Builder(ctx);
+        upd.setTitle("[ACOM] has an update!");
+        upd.setMessage("[ACOM] has an update!\nDo you want to update it now?\nCurrent version: " + version + "\nNew version: " + newUpdate + "\nThis Update has a Bug fixes on button");
+        upd.setNegativeButton("Nope", new android.content.DialogInterface.OnClickListener() {
+            onClick: function(par1) {
+            dialog.dismiss(); 
+   }
+        });
+        upd.setPositiveButton("Update!", new android.content.DialogInterface.OnClickListener() {
+            onClick: function(par1) {
+                var ru  = new java.lang.Runnable() {
+                    run: function() {
+                        try {
+                            var urls = new java.net.URL(" https://raw.githubusercontent.com/XxCoderGamerxX/Advanced-Mods/master/Coder_G%20Optifine.js ");
+                            var check = urls.openConnection();
+                            check.setRequestMethod("GET");
+                            check.setDoOutput(true);
+                            check.connect();
+                            check.getContentLength();
+                            var script = check.getInputStream();
+                            var typeb = java.lang.reflect.Array.newInstance(java.lang.Byte.TYPE, 1024);
+                            var byteCount = 0;
+                            while((byteCount = script.read(typeb)) != -1) { 
+                                updateMod += new java.lang.String(typeb, 0, byteCount);               
+                            }
+                            var modpeFolder = ctx.getDir("modscripts", 0);
+                            var modpeFile = new java.io.File(modpeFolder, "Advanced Coder_G OPTIFINE HD Mod.js");
+                            var update = new java.io.PrintWriter(modpeFile);
+                            update.write(updateMod);
+                            update.flush();
+                            update.close();
+                             
+                            try {
+                                net.zhuoweizhang.mcpelauncher.ScriptManager.setEnabled(modpeFile, false);
+                                net.zhuoweizhang.mcpelauncher.ScriptManager.setEnabled(modpeFile, true);
+                                clientMessage("[ACOM] The OPTIFINE Mod has been downloaded and enabled!");
+                                   
+                            }
+                            catch(err) {
+                                clientMessage("[ACOM] Error: \n" + err);
+                            }
+                        }
+                        catch(err) {
+                            clientMessage("[ACOM] Error: \n" + err);
+                        }
+                    }
+                }
+                var threadt = new java.lang.Thread(ru);
+                threadt.start();
+            }
+        });
+        var dialog = upd.create();
+        dialog.show() 
+    }
+    catch(err) {
+        clientMessage("[ACOM] Error: \n" + err);
+    }
+}
+  
+ function modTick(){
+ if(checkForUpdate==false) {
+        print("Checking for an update");
+        ctx.runOnUiThread(new java.lang.Runnable({
+            run: function() {
+                try {
+                    checkVersion();
+                }
+                catch(err) {
+                    clientMessage("[ACOM] Error: \n"+err);
+                }
+            }
+        }));
+        checkForUpdate=true;
+    }
+    if(updateWindow) {
+        ctx.runOnUiThread(new java.lang.Runnable({
+            run: function() {
+                try {
+                    updateVersion();
+                }
+                catch(err) {
+                    clientMessage("[ACOM] Error: \n" + err);
+                }
+            }
+        }));
+        updateWindow=false;
+    } 
+ }  
+//END OF UPDATE CODE
+
 var c1 = 5000;
 var c2 = 100;
 function modTick() {
